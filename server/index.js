ea63da61bc9409c047b0d2969b65c10d39d68332
Om <omtita971@gmail.com>
2025-05-22 02:13:53 +0530

    Backend Build Command

--- server/index.js
+++ server/index.js
@@ -122,5 +122,5 @@ app.post('/suggest', async (req, res, next) => {
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
